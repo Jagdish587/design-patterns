bf52eda07292557e0c97e882985b5634e70b37e7
Jagdish Tirumala <t.jag587@gmail.com>
2020-04-19 18:43:37 +0530

    Protottype Pattern

--- CreationalPatterns/Prototype-Pattern.cpp
+++ CreationalPatterns/Prototype-Pattern.cpp
@@ -1,161 +1,76 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
 using namespace std;
 
-////////////////////////////// Report ////////////////////////////////////////////////
 
-// This is Report Interface Class
-
-class Report
-{
+////////////////////////////////////////////////////////////////////
+/// \brief The Stooge class
+///
+class Stooge {
 public:
-    virtual Report* clone() = 0;
-    virtual void display() = 0;
-
-    virtual ~Report() {}
-
+    virtual Stooge* clone() = 0;
+    virtual void slap_stick() = 0;
 };
 
 
-class ExcelReport : public Report
-{
+class Larry : public Stooge {
 public:
-
-    Report* ptr;
-
-    Report* clone() {
-        ptr = new ExcelReport();
-        return ptr;
-    }
-
-    void display() {
-        cout<<"i am ExcelReport \n";
-    }
-
-    ~ ExcelReport() {
-        delete ptr;
-    }
-
+    Stooge*   clone() { return new Larry; }
+    void slap_stick() {
+        cout << "Larry: poke eyes\n"; }
 };
 
-
-class PPTReport : public Report
-{
+class Moe : public Stooge {
 public:
-    Report* ptr ;
-
-    Report* clone() {
-        ptr = new PPTReport();
-        return ptr;
-    }
-
-    void display() {
-        cout<<"i am PPTReport \n";
-    }
-
-    ~ PPTReport() {
-        delete ptr;
-    }
-
+    Stooge*   clone() { return new Moe; }
+    void slap_stick() {
+        cout << "Moe: slap head\n"; }
 };
 
-class WordReport : public Report
-{
+class Curly : public Stooge {
 public:
-
-     Report* ptr ;
-
-    Report* clone() {
-        ptr = new WordReport();
-        return ptr;
-    }
-
-    void display() {
-        cout<<"i am WordReport \n";
-    }
-
-    ~ WordReport() {
-        delete ptr;
-    }
-
+    Stooge*   clone() { return new Curly; }
+    void slap_stick() {
+        cout << "Curly: suffer abuse\n"; }
 };
 
-///////////////////////////// End //////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 
-
-class ReportMangaer
-{
+class Factory {
 public:
-    static Report* rptr1 ;
-    static Report* rptr2 ;
-    static Report* rptr3 ;
-
-    static void initlize()
-    {
-        rptr1 = new ExcelReport();
-        rptr2 = new PPTReport();
-        rptr3 = new WordReport();
-    }
-
-    static Report* createReport(int choice) {
-
-        if(choice == 1)
-            rptr1->clone();
-        else if(choice == 2)
-            rptr2->clone();
-        else if(choice == 3){
-            rptr3->clone();
-        }
-    }
-
-    ~ ReportMangaer()
-    {
-        delete rptr1;
-        delete rptr2;
-        delete rptr3;
-    }
-
+    static Stooge* make_stooge( int choice );
+private:
+    static Stooge* s_prototypes[4];
 };
 
-Report* ReportMangaer::rptr1 = nullptr;
-Report* ReportMangaer::rptr2 = nullptr;
-Report* ReportMangaer::rptr3 = nullptr;
-
-struct Destruct
-{
-    void operator()(Report *a) const {
-    delete a;
-    }
+Stooge* Factory::s_prototypes[] = {
+    0, new Larry, new Moe, new Curly
 };
+Stooge* Factory::make_stooge( int choice ) {
+    return s_prototypes[choice]->clone();
+}
 
-int main()
-{
-    vector<Report*> myReportvector(4);
-    int choice;
-    int iter =0;
+/////////////////////////////////////////////////////////////////////////
 
-    ReportMangaer::initlize();
+int main() {
+    vector<Stooge*> roles;
+    int             choice;
 
     while (true) {
-        cout<<"enter your choice 1 or 2 or 3";
-        cin>>choice;
-
-        if(choice == 1 || choice == 2 || choice ==3) {
-            cout<<"\nentered choice = "<<choice;
-            myReportvector[1] = ReportMangaer::createReport(choice);
-            myReportvector[1]->display();
-        } else
+        cout << "Larry(1) Moe(2) Curly(3) Go(0): ";
+        cin >> choice;
+        if (choice == 0)
             break;
-
-        iter = iter + 1;
+        roles.push_back(
+            Factory::make_stooge( choice ) );
     }
 
-    Destruct dobj;
-    // this calls Destruct::operator()
-    std::for_each(myReportvector.begin(), myReportvector.end(), dobj);
-
-
-    return 0;
+    for (int i=0; i < roles.size(); ++i)
+        roles[i]->slap_stick();
+    for (int i=0; i < roles.size(); ++i)
+        delete roles[i];
 }
+
+
+
+
